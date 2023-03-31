f0ab9a65c219b562d9ab05e29c33d42981e00a73
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2023-03-31 17:23:27 +0200

    Set ImagePullPolicy to PullAlways

--- pkg/resources/unleash.go
+++ pkg/resources/unleash.go
@@ -174,7 +174,7 @@ func DeploymentForUnleash(unleash *unleashv1.Unleash, scheme *runtime.Scheme) (*
 					Containers: []corev1.Container{{
 						Image:           ImageForUnleash(unleash),
 						Name:            "unleash",
-						ImagePullPolicy: corev1.PullIfNotPresent,
+						ImagePullPolicy: corev1.PullAlways,
 						// Ensure restrictive context for the container
 						// More info: https://kubernetes.io/docs/concepts/security/pod-security-standards/#restricted
 						SecurityContext: &corev1.SecurityContext{
