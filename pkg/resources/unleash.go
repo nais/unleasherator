b1930844d1f1e89c9fefa73164a78c2f2fddc897
Carl Hedgren <carl.hedgren@nav.no>
2023-04-11 10:27:17 +0200

    Delete unused resource
    closes #27
    also closes #26 as a drive-by cleaning operation, the changes live
    somehwere elese on main

--- pkg/resources/unleash.go
+++ pkg/resources/unleash.go
@@ -9,7 +9,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/intstr"
