1dbd6a28e15f7b16b3c79fcef064987304f74547
Carl Hedgren <carl.hedgren@nav.no>
2023-04-21 14:50:12 +0200

    :hocho: commented out book code

--- controllers/unleash_controller_test.go
+++ controllers/unleash_controller_test.go
@@ -54,63 +54,9 @@ var _ = Describe("Unleash controller", func() {
 			// Let's make sure our Schedule string value was properly converted/handled.
 			Expect(createUnleash.Spec.Size).Should(Equal(int32(1)))
 
+			By("This fails, always")
 			Expect(false).Should(BeTrue())
 
-			//By("By checking the CronJob has zero active Jobs")
-			//Consistently(func() (int, error) {
-			//	err := k8sClient.Get(ctx, cronjobLookupKey, createdCronjob)
-			//	if err != nil {
-			//		return -1, err
-			//	}
-			//	return len(createdCronjob.Status.Active), nil
-			//}, duration, interval).Should(Equal(0))
-
-			//By("By creating a new Job")
-			//testJob := &batchv1.Job{
-			//	ObjectMeta: metav1.ObjectMeta{
-			//		Name:      JobName,
-			//		Namespace: CronjobNamespace,
-			//	},
-			//	Spec: batchv1.JobSpec{
-			//		Template: v1.PodTemplateSpec{
-			//			Spec: v1.PodSpec{
-			//				// For simplicity, we only fill out the required fields.
-			//				Containers: []v1.Container{
-			//					{
-			//						Name:  "test-container",
-			//						Image: "test-image",
-			//					},
-			//				},
-			//				RestartPolicy: v1.RestartPolicyOnFailure,
-			//			},
-			//		},
-			//	},
-			//	Status: batchv1.JobStatus{
-			//		Active: 2,
-			//	},
-			//}
-
-			//// Note that your CronJob’s GroupVersionKind is required to set up this owner reference.
-			//kind := reflect.TypeOf(cronjobv1.CronJob{}).Name()
-			//gvk := cronjobv1.GroupVersion.WithKind(kind)
-
-			//controllerRef := metav1.NewControllerRef(createdCronjob, gvk)
-			//testJob.SetOwnerReferences([]metav1.OwnerReference{*controllerRef})
-			//Expect(k8sClient.Create(ctx, testJob)).Should(Succeed())
-
-			//By("By checking that the CronJob has one active Job")
-			//Eventually(func() ([]string, error) {
-			//	err := k8sClient.Get(ctx, cronjobLookupKey, createdCronjob)
-			//	if err != nil {
-			//		return nil, err
-			//	}
-
-			//	names := []string{}
-			//	for _, job := range createdCronjob.Status.Active {
-			//		names = append(names, job.Name)
-			//	}
-			//	return names, nil
-			//}, timeout, interval).Should(ConsistOf(JobName), "should list our active job %s in the active jobs list in status", JobName)
 		})
 	})
 
