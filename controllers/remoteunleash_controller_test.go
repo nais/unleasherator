94e44b3a3cb017bad2f252256996458567fcab29
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2023-05-09 10:14:02 +0200

    Add positive test case

--- controllers/remoteunleash_controller_test.go
+++ controllers/remoteunleash_controller_test.go
@@ -6,6 +6,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 
@@ -16,8 +17,9 @@ var _ = Describe("RemoteUnleash controller", func() {
 
 	// Define utility constants for object names and testing timeouts/durations and intervals.
 	const (
-		RemoteUnleashName      = "test-remote-unleash"
 		RemoteUnleashNamespace = "default"
+		RemoteUnleashServerURL = "https://unleash.nais.io"
+		RemoteUnleashToken     = "test"
 
 		timeout  = time.Second * 10
 		duration = time.Second * 10
@@ -26,8 +28,11 @@ var _ = Describe("RemoteUnleash controller", func() {
 
 	Context("When creating a RemoteUnleash", func() {
 		It("Should fail if the secret does not exist", func() {
-			By("By creating a new RemoteUnleash")
 			ctx := context.Background()
+
+			RemoteUnleashName := "test-unleash-fail-secret"
+
+			By("By creating a new RemoteUnleash")
 			remoteUnleash := &unleashv1.RemoteUnleash{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "unleash.nais.io/v1",
@@ -39,7 +44,7 @@ var _ = Describe("RemoteUnleash controller", func() {
 				},
 				Spec: unleashv1.RemoteUnleashSpec{
 					Server: unleashv1.RemoteUnleashServer{
-						URL: "https://unleash.nais.io",
+						URL: RemoteUnleashServerURL,
 					},
 					AdminSecret: unleashv1.RemoteUnleashSecret{
 						Name: "unleasherator-not-exist",
@@ -64,6 +69,68 @@ var _ = Describe("RemoteUnleash controller", func() {
 			Expect(createdRemoteUnleash.Status.Conditions[1].Status).To(Equal(metav1.ConditionFalse))
 			Expect(createdRemoteUnleash.Status.Conditions[1].Reason).To(Equal("Reconciling"))
 			Expect(createdRemoteUnleash.Status.Conditions[1].Message).To(Equal("Failed to get admin token secret"))
+			Expect(createdRemoteUnleash.IsReady()).To(BeFalse())
+
+			By("By deleting the RemoteUnleash")
+			Expect(k8sClient.Delete(ctx, createdRemoteUnleash)).Should(Succeed())
+		})
+
+		It("Should succeed when it can connect to Unleash", func() {
+			ctx := context.Background()
+			RemoteUnleashName := "test-unleash-success"
+
+			By("By creating a new Unleash secret")
+			secret := &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Secret",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "unleasherator-test",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"token": []byte(RemoteUnleashToken),
+				},
+			}
+			Expect(k8sClient.Create(ctx, secret)).Should(Succeed())
+
+			By("By creating a new RemoteUnleash")
+			remoteUnleash := &unleashv1.RemoteUnleash{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "unleash.nais.io/v1",
+					Kind:       "RemoteUnleash",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      RemoteUnleashName,
+					Namespace: RemoteUnleashNamespace,
+				},
+				Spec: unleashv1.RemoteUnleashSpec{
+					Server: unleashv1.RemoteUnleashServer{
+						URL: RemoteUnleashServerURL,
+					},
+					AdminSecret: unleashv1.RemoteUnleashSecret{
+						Name: secret.GetName(),
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, remoteUnleash)).Should(Succeed())
+
+			remoteUnleashLookupKey := types.NamespacedName{Name: RemoteUnleashName, Namespace: RemoteUnleashNamespace}
+			createdRemoteUnleash := &unleashv1.RemoteUnleash{}
+
+			Eventually(func() ([]metav1.Condition, error) {
+				err := k8sClient.Get(ctx, remoteUnleashLookupKey, createdRemoteUnleash)
+				if err != nil {
+					return nil, err
+				}
+				return createdRemoteUnleash.Status.Conditions, nil
+			}, timeout, interval).Should(HaveLen(2))
+
+			Expect(createdRemoteUnleash.Status.Conditions[1].Type).To(Equal(typeAvailableUnleash))
+			Expect(createdRemoteUnleash.Status.Conditions[1].Status).To(Equal(metav1.ConditionTrue))
+			Expect(createdRemoteUnleash.Status.Conditions[1].Reason).To(Equal("Reconciling"))
+			Expect(createdRemoteUnleash.Status.Conditions[1].Message).To(Equal("Reconciled successfully"))
 			Expect(createdRemoteUnleash.IsReady()).To(BeFalse())
 		})
 	})
