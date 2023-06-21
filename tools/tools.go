e9354be03618965eb0c1a43462efbf87eadb0dc1
Carl Hedgren <Reasonable-Solutions@users.noreply.github.com>
2023-06-21 09:11:27 +0200

    Add tools.go to pin development dependencies (#139)

---
+++ tools/tools.go
@@ -0,0 +1,14 @@
+//go:build tools
+// +build tools
+
+// This file will never be built, but `go mod tidy` will see the packages
+// imported here as dependencies and not remove them from `go.mod`.
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/nais/helmify/cmd/helmify"
+	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+)
