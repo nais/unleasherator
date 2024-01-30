be748a1a54f34954401ac8c2a664a728ce05f536
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-01-30 11:14:58 +0100

    Refactor otel resource metadata and test hadning (#317)
    * Add test case for hostport parsing logic
    * Refactor otel resources

--- pkg/o11y/tracing_test.go
+++ pkg/o11y/tracing_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/nais/unleasherator/pkg/config"
 	"github.com/stretchr/testify/assert"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 )
 
 func TestNewTraceExporter(t *testing.T) {
@@ -126,4 +127,69 @@ func TestNewTraceExporter(t *testing.T) {
 		assert.Nil(t, exporter)
 		assert.EqualError(t, err, fmt.Sprintf("unsupported otlp exporter protocol %q", cfg.OpenTelemetry.ExporterOtlpProtocol))
 	})
+}
+
+func TestHostportSchemeFromUrl(t *testing.T) {
+	tests := []struct {
+		name     string
+		rawUrl   string
+		expected string
+	}{
+		{
+			name:     "HTTP URL without port",
+			rawUrl:   "http://example.com",
+			expected: "example.com:80, http",
+		},
+		{
+			name:     "HTTPS URL without port",
+			rawUrl:   "https://example.com",
+			expected: "example.com:443, https",
+		},
+		{
+			name:     "URL with custom port",
+			rawUrl:   "http://example.com:8080",
+			expected: "example.com:8080, http",
+		},
+		{
+			name:     "URL with custom port and HTTPS scheme",
+			rawUrl:   "https://example.com:8443",
+			expected: "example.com:8443, https",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hostport, scheme, err := hostportSchemeFromUrl(test.rawUrl)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, fmt.Sprintf("%s, %s", hostport, scheme))
+		})
+	}
+}
+func TestResourceFromConfig(t *testing.T) {
+	cfg := &config.Config{
+		ClusterName:  "test-cluster",
+		PodName:      "test-pod",
+		PodNamespace: "test-namespace",
+	}
+
+	resource, err := resourceFromConfig(cfg)
+	assert.NoError(t, err)
+
+	for _, attribute := range resource.Attributes() {
+		switch attribute.Key {
+		case semconv.ServiceNameKey:
+			assert.Equal(t, "unleasherator-test-cluster", attribute.Value.AsString())
+		case semconv.ServiceInstanceIDKey:
+			assert.Equal(t, "test-pod", attribute.Value.AsString())
+		case semconv.ServiceNamespaceKey:
+			assert.Equal(t, "test-namespace", attribute.Value.AsString())
+		case semconv.K8SClusterNameKey:
+			assert.Equal(t, "test-cluster", attribute.Value.AsString())
+		case semconv.TelemetrySDKNameKey:
+		case semconv.TelemetrySDKLanguageKey:
+		case semconv.TelemetrySDKVersionKey:
+		default:
+			assert.Fail(t, fmt.Sprintf("unexpected attribute %q", attribute.Key))
+		}
+	}
 }
