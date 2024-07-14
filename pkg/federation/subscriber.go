2da019f8a8fe8bc4a9a20a4925d28b69d997781b
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-07-14 20:10:31 +0000

    Update go.opentelemetry.io/otel/semconv/v1.26.0

--- pkg/federation/subscriber.go
+++ pkg/federation/subscriber.go
@@ -10,7 +10,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/propagation"
-	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/protobuf/proto"
 	corev1 "k8s.io/api/core/v1"
--- pkg/o11y/tracing_test.go
+++ pkg/o11y/tracing_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/nais/unleasherator/pkg/config"
 	"github.com/stretchr/testify/assert"
-	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
 func TestNewTraceExporter(t *testing.T) {
